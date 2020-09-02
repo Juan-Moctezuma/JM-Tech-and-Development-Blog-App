b38323b31141a96d28583fd83fe07253924f1f82
Juan-Moctezuma <juan.moctezuma091@gmail.com>
2020-09-02 11:10:08 -0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,9 +1,10 @@
 let mongoose = require("mongoose");
 let passportLocalMongoose = require("passport-local-mongoose");
 
+// USER & PASSWORD MONGODB SCHEMA / STRUCTURE
 let UserSchema = new mongoose.Schema({
 	username: String,
-    password: String
+    	password: String
 });
 
 UserSchema.plugin(passportLocalMongoose);
